e82acfda14e9c5abd6be37c4329d3860a3119a74
slydragonn <alogo18m@gmail.com>
2024-10-18 14:16:43 -0500

    chenge c file for cpp

--- algoritmos_1/TI7_vectores/ejercicio_3.cpp
+++ algoritmos_1/TI7_vectores/ejercicio_3.cpp
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-void llenarConMultiplosDeTres(int[]);
-int sumarMultiplos(int[]);
+void llenarConMultiplosDeTres(int *);
+int sumarMultiplos(int *);
 
-void main()
+int main()
 {
   int multiplos3[10] = {0};
   int suma;
@@ -11,28 +12,27 @@ void main()
   llenarConMultiplosDeTres(multiplos3);
   suma = sumarMultiplos(multiplos3);
 
-  printf("La suma de los multiplos de 3 es: %d\n", suma);
+  cout << "La suma de los multiplos de 3 es: " << suma << endl;
 }
 
-void llenarConMultiplosDeTres(int multiplos[])
+void llenarConMultiplosDeTres(int *multiplos)
 {
   int multiplo = 3;
 
   for(int i = 0; i < 10; i++)
   {
-    printf("Multiplo: %d\n", multiplo);
+    cout << "Multiplo: " << multiplo << endl;
     multiplos[i] = multiplo;
     multiplo += 3;
   }
 }
 
-int sumarMultiplos(int multiplos[])
+int sumarMultiplos(int *multiplos)
 {
   int resultado = 0;
 
   for(int i = 0; i < 10; i++)
   {
-    printf("Elemento: %d\n", multiplos[i]);
     resultado += multiplos[i];
   }
 
