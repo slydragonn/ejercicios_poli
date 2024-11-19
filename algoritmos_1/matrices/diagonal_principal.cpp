43b76e4149ff5acb18892e24c0628232200e9c0b
slydragonn <alogo18m@gmail.com>
2024-11-19 14:14:30 -0500

    matrices, diagonal secundaria

--- algoritmos_1/matrices/diagonal_principal.cpp
+++ algoritmos_1/matrices/diagonal_principal.cpp
@@ -5,6 +5,7 @@ int matriz[50][50] = {0};
 
 void llenar_matriz(int, int);
 void diagonal_principal(int, int);
+void diagonal_secundaria(int, int);
 
 main()
 {
@@ -19,6 +20,8 @@ main()
   llenar_matriz(F, C);
   cout << "Diagonal Principal: " << endl;
   diagonal_principal(F, C);
+  cout << "Diagonal Secundaria: " << endl;
+  diagonal_secundaria(F, C);
 }
 
 void llenar_matriz(int fil, int col)
@@ -46,6 +49,26 @@ void diagonal_principal(int fil, int col)
     for(i = 0; i <= fil - 1; i++)
     {
       cout << matriz[i][i] << "\t";
+    }
+    cout << endl;
+  }
+  else
+  {
+    cout << "La matriz no es cuadrada" << endl;
+  }
+}
+
+void diagonal_secundaria(int fil, int col)
+{
+  int i, j;
+
+  if(fil == col)
+  {
+    j = col;
+    for(i = 0; i <= fil - 1; i++)
+    {
+      j -= 1;
+      cout << matriz[i][j] << "\t";
     }
     cout << endl;
   }
